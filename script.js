57b83fa2b47911dd071e4120ca9e1d84c463efb6
Kent <kenttechnical@gmail.com>
2022-08-03 12:53:35 +0200

    Fetch Api

---
+++ script.js
@@ -0,0 +1,9 @@
+// Fetch Api
+// Fetch(endpoint), .then once it completes, return the response.json because response.json is a promise
+// And promises can't be directly access, to access it, another .then onto fetch is used with a callback function
+// The argument used in the second .then will hold the response json objects
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then((response) =>{ return response.json()})
+  .then((json) => {console.log(json)})
+  .catch((err) => {console.log("Error occured ", err)})
